d265acdae937a65eee9204cab623edc32bd1f46a
Ruben Cruz <acostakaku420@gmail.com>
2024-08-01 12:15:19 -0500

    cambios realizados

--- app/src/main/java/com/example/location_feature/model/horario.kt
+++
@@ -1,9 +0,0 @@
-package com.example.location_feature.model
-data class Horario (
-    val id : String ,
-    val horaInicio: String,
-    val horaFin: String,
-    var idRuta: String,
-    var idZona: String,
-    var dias: String
-)
