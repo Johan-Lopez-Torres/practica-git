07f49e83b37ebbf52f876b54af604d817e23f0d0
bryan <barteagame@gmail.com>
2024-08-04 20:00:09 -0500

    listo - login - create merge

--- app/src/main/java/com/example/location_feature/network/dependencyInjection/FirebaseModule.kt
+++ app/src/main/java/com/example/location_feature/network/dependencyInjection/FirebaseModule.kt
@@ -1,4 +1,4 @@
-package com.example.location_feature.dependencyInjection
+package com.example.location_feature.network.dependencyInjection
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
--- app/src/main/java/com/example/location_feature/network/dependencyInjection/RepositoryModule.kt
+++ app/src/main/java/com/example/location_feature/network/dependencyInjection/RepositoryModule.kt
@@ -1,4 +1,4 @@
-package com.example.location_feature.dependencyInjection
+package com.example.location_feature.network.dependencyInjection
 import com.example.location_feature.network.FirebaseAuthRepositoryImpl
 
 import com.example.location_feature.domain.repository.AuthRepository
